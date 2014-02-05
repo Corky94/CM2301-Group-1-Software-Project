a25b50c527351a5a878c0b7563db0bf6685f8e3d
Marc Cork <Marc.cork@gmail.com>
2014-02-05 13:03:15 +0000

    In progress user file, haven't got much time atm, but give me a few days

---
+++ User.java
@@ -0,0 +1,15 @@
+
+// This is a basic structure of the User class, there is still much to be done and al the methods to be implimented. 
+
+public class User 
+
+
+	private int userId;
+	private String password;
+	private Messages[] savedMessages;
+	private Messages[] newMessages; 
+	
+
+	public User {
+	
+	}
