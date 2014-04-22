23c2b8fbd44c89b1d1863e693b9ce80e0f1f8181
Marc Cork <marc.cork@gmail.com>
2014-04-22 18:58:32 +0100

    Sorted the authentication I think

--- Client/Crypto/Authentication.java
+++ Client/Crypto/Authentication.java
@@ -6,18 +6,55 @@
 
 package Crypto;
 
+import java.security.Key;
+
 /**
  *
  * @author maxchandler
  */
 public class Authentication {
     
-    Authentication(){
-    
+    public static byte[] auth(String nodeId){
+        //This method is called to connect to a node, if it has the right auth
+        //it will return it, if not it will generate a request.
+        if(doesTicketExist(nodeId) == true){
+            //get the ticket and encrypt it ready to be send to the node as authentication
+            Ticket t = AuthenticatedList.getNodeAuthentication(nodeId);
+            return Encryption.encryptAuth(t.getNodePublicKey(), t);
+        }else{
+            //generates a new ticket with a request in, that is then encrypted and 
+            //ready to be sent to the node
+            //            
+            // --- Ticket Content ---
+            //clientId = clientID;
+            //nodeId = nodeID;
+            //otp = null;
+            //clientPublicKey = clientPublicKey;
+            //nodePublicKey = null; 
+            //is_challenge = true; 
+            Key nodePublicKey = getNodePublicKey(nodeId); //HOW DO I CONNECT TO AN ID SERVER AND GET A PUBLIC KEY?
+            return Encryption.encryptAuth(nodePublicKey, Ticket.generateRequest(nodeId));
+        } 
     }
     
-    public static byte[] handleChallenge(char[] localPassword, byte[] challenge){
-        return Encryption.encryptString(Encryption.decryptString(challenge, localPassword), NEEDS_USER_ID);
+    //method to be called as ticket is returned from node
+    public static byte[] handleChallenge(byte[] challenge){
+        Ticket t = (Ticket) Encryption.decryptAuth(challenge);        
+        if(t == null){    
+            throw new RuntimeException("Ticket returned null");
+        }
+        //Ticket has come back, and is no longer a challenge, however the node does not know this
+        //so we'll return one copy that will be treated as a challenge returned
+        Ticket newTicket = new Ticket(t);
+        newTicket.setChallenge(false);
+        AuthenticatedList.addAuth(newTicket);
+        //And return the decrypted ticket ready to send back to the server.
+        return Encryption.encryptAuth(t.getNodePublicKey(), t);
     }
     
+    private static boolean doesTicketExist(String serverId){
+        //tidy up the auth list before we use it, and remove expired auth
+        AuthenticatedList.removeExpiredAuth();
+        return AuthenticatedList.exists(serverId);
+    }
 }
