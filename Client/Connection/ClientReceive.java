8180d1d5e4e9bb349135914cd3b5627439dc9a7b
Max Chandler <maxstewartchandler@gmail.com>
2014-04-23 18:39:20 +0100

    cleanup

--- Client/Connection/ClientReceive.java
+++ Client/Connection/ClientReceive.java
@@ -7,130 +7,87 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.ObjectOutputStream;
 import java.io.ObjectInputStream;
-import java.net.ConnectException;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.security.*;
 import java.security.spec.*;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.net.ssl.SSLSocket;
 
 public class ClientReceive {
-	private static boolean  debug = true;
-	private static byte[] sender = "sender".getBytes();
+    private static boolean  debug = true;
+    private static byte[] sender = "sender".getBytes();
 
-
-        
-        
-	public static Message[] receive(String Id, char[] localPassword) {
-		try{
-			Message[] allMessages;
-			Message m = new Message();
-			m.setSender(null);
-			m.setReceiver(Id);
-			m.setMessage(null);
-                        SSLSocket s;
-
-
-			while (true){
-					ClientSSL c = Console.User.clissl;
-
-                            
-                            s = c.main(12346);
-                               
-					
-
-
-                            System.out.println(s);
-                            OutputStream os = s.getOutputStream();  
-                            ObjectOutputStream oos = new ObjectOutputStream(os);  
-                            oos.writeObject(m);   
-
-
-                            //get reply from server
-                            InputStream is = s.getInputStream();  
-                            ObjectInputStream ois = new ObjectInputStream(is);
-                            allMessages = (Message[]) ois.readObject();
-                            //if(debug) System.out.println(m);
-                            int i = allMessages.length;
-
-
-                            oos.close();  
-                            os.close(); 
-                            is.close();
-                            ois.close();
-                            s.close(); 
-                            return allMessages;
-			}
-		}
-		catch(Exception e){
-			Logger.getLogger(ClientReceive.class.getName()).log(Level.SEVERE, null, e);
-		}
-			
-
-		return null;
-		
-			
-	}  
-        
-        private static byte[] getKeyFromServer(Message m, char[] localPassword){  
-            Message message = new Message();
+    public static Message[] receive(String Id, char[] localPassword) {
+        try{
+            Message[] allMessages;
+            Message m = new Message();
+            m.setSender(null);
+            m.setReceiver(Id);
+            m.setMessage(null);
             SSLSocket s;
-            try {
-                
+            while (true){
                 ClientSSL c = Console.User.clissl;
-
-               
                 s = c.main(12346);
-                 
-                
-                OutputStream os = s.getOutputStream(); 
-                 
-                ObjectOutputStream oos = new ObjectOutputStream(os);
-                oos.writeObject(m);
 
-                
+                System.out.println(s);
+                OutputStream os = s.getOutputStream();  
+                ObjectOutputStream oos = new ObjectOutputStream(os);  
+                oos.writeObject(m);   
+
+                //get reply from server
                 InputStream is = s.getInputStream();  
                 ObjectInputStream ois = new ObjectInputStream(is);
+                allMessages = (Message[]) ois.readObject();
+                //if(debug) System.out.println(m);
+                int i = allMessages.length;
 
-
-                message = (Message) ois.readObject();
-                
+                oos.close();  
+                os.close(); 
                 is.close();
                 ois.close();
-                
-                
-                return message.getKey();
-                
- 
-            } catch (IOException ex) {
-                Logger.getLogger(ClientSend.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (ClassNotFoundException ex) {
-                Logger.getLogger(ClientReceive.class.getName()).log(Level.SEVERE, null, ex);
+                s.close(); 
+                return allMessages;
             }
-            System.out.println("TEST");
-            
-            
+        }catch(IOException | ClassNotFoundException e){
+            Logger.getLogger(ClientReceive.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return null;		
+    }  
+        
+    private static byte[] getKeyFromServer(Message m){  
+        Message message = new Message();
+        SSLSocket s;
+        try {
+            ClientSSL c = Console.User.clissl;
+            s = c.main(12346);
+            OutputStream os = s.getOutputStream(); 
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            oos.writeObject(m);
+            InputStream is = s.getInputStream();  
+            ObjectInputStream ois = new ObjectInputStream(is);
+            message = (Message) ois.readObject();             
+            is.close();
+            ois.close();
+            return message.getKey();
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(ClientSend.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("TEST");
+        return null;
+    }
 
-            return null;
-}
-
-    //This needs to be reworked
     public static PublicKey getKey(String id, char[] pass) {
         Message m = new Message();
         m.setReceiver(id);
         m.setNeedingKey(true);
-        
-        byte[] key = getKeyFromServer(m, pass);
+        byte[] key = getKeyFromServer(m);
         try {
             X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(key);
             KeyFactory kf = KeyFactory.getInstance("RSA");
             PublicKey pk = kf.generatePublic(pubKeySpec);
             return pk;
         }
-        catch (Exception ex) {
+        catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
             throw new RuntimeException(ex);
         }
     }
